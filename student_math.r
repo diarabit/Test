c5d56b1a6f1d48740417ccbe61676a13a485ba80
diarabit <diarabit@naver.com>
2019-05-29 10:59:56 +0900

    Update student_math.r

--- student_math.r
+++ student_math.r
@@ -1,5 +1,5 @@
 setwd("D:/BigData R분석 고급/student")
-d1=read.table("student-mat.csv",sep=",",header=TRUE)
+d1=read.table("student-mat.csv", sep=",", header=TRUE)
 #d2=read.table("student-por.csv",sep=";",header=TRUE)
 
 #d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
@@ -18,49 +18,36 @@ library(car)
 
 boxplot(d1)
 colnames(d1)
+str(d1)
 
-data <- select(d1, -'school', -'famsize', -'reason', 'freetime')
-data <- data[data$absences < 20, ]
+data <- d1
+#data <- select(d1, -'school', -'famsize')
+data <- data[data$absences < 10, ]
+str(data)
 
-
-set.seed(1111) # 0.34, 0.41
-set.seed(4856) # 0.35, 0.36
-set.seed(292100) # 0.29, 0.55
-set.seed(9436) # 0.32, 0.52
-set.seed(3075) # 0.33, 0.45
-set.seed(0075) # 0.33, 0.38
-set.seed(9367) # 0.32, 0.54
+set.seed(9367)
 index <- sample(nrow(data), nrow(data)*0.9)
 
-data$age <- as.factor(data$age)
-data$Medu <- as.factor(data$Medu)
-data$Fedu <- as.factor(data$Fedu)
-data$traveltime <- as.factor(data$traveltime)
-data$studytime <- as.factor(data$studytime)
-data$failures <- as.factor(data$failures)
-data$famrel <- as.factor(data$famrel)
-data$freetime <- as.factor(data$freetime)
-data$goout <- as.factor(data$goout)
-data$Dalc <- as.factor(data$Dalc)
-data$Walc <- as.factor(data$Walc)
-data$health <- as.factor(data$health)
+fcol <- c('age', 'Medu', 'Fedu', 'traveltime', 'studytime', 'failures', 'famrel', 'freetime', 'goout', 
+          'Dalc', 'Walc', 'health')
 
+data[, fcol] <- lapply(data[, fcol], as.factor )
 
 train <- data[index,]
 test <- data[-index, ]
 
-train$G2 <- NULL
-train$G3 <- NULL
-test$G2 <- NULL
-test$G3 <- NULL
+#train$G2 <- NULL
+#train$G3 <- NULL
+#test$G2 <- NULL
+#test$G3 <- NULL
 
 
 # 선형회귀
-fit.full <- lm(G1 ~ ., train)
-fit.min <- lm(G1 ~ 1, train)
+fit.full <- lm(G1+G2+G3 ~ ., train)
+fit.min <- lm(G1+G2+G3 ~ 1, train)
 step_model <- step(fit.min, direction="both", scope=list(upper=fit.full,lower=fit.min))
 summary(step_model)
-
+coef(step_model)
 
 #step_model$anova
 
@@ -69,6 +56,9 @@ confint(step_model)
 
 # 표준화 잔차의 분포를 시각적으로 확인하기
 plot(rstandard(step_model))
+hist(rstandard(step_model))
+
+#정규성을 가지고 있는가?
 
 # 정규성 검증
 shapiro.test(rstandard(step_model))
@@ -94,7 +84,7 @@ summary(model.pred)
 
 # 정확도 계산
 actuals_preds <-
-  data.frame(cbind(actuals=test$G1, prediction=model.pred))
+  data.frame(cbind(actuals=test$G1+test$G2+test$G3, prediction=model.pred))
 
 correlation_accuracy <- cor(actuals_preds) 
 correlation_accuracy
